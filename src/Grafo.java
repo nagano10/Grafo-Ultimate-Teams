c2226e1f82f3ba22f8071477705773480a5cfbad
nagano10 <eduardo.naganolacerda@gmail.com>
2023-10-28 16:01:52 -0300

    Foram adicionados novos metodos e um menu de interacao com o usuario.

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Grafo {
@@ -10,11 +11,13 @@ public class Grafo {
         this.compatibilidades = new ArrayList<>();
     }
 
+    //Adiciona um jogador, passando parametros de Nome, Nacionalidade e Clube.
     public void adicionarJogador(String nome, String nacionalidade, String clube) {
         Jogador jogador = new Jogador(nome, nacionalidade, clube);
         jogadores.add(jogador);
     }
 
+    //Calcula a Afinidade entre todos os jogadores.
     public void calcularAfinidade() {
         for (int i = 0; i < jogadores.size(); i++) {
             Jogador jogador1 = jogadores.get(i);
@@ -26,6 +29,10 @@ public class Grafo {
         }
     }
 
+    //Classifica as afinidades em peso. Criterios levados em consideracao
+    //Afinidade 1: os jogadores nao jogam no mesmo time nem sao da mesma nacionalidade.
+    //Afinidade 2: Os jogadores jogam no mesmo time ou sao da mesma nacionalidade.
+    //Afinidade 3: Os jogadores jogam no mesmo time e sao da mesma nacionalidade.
     private int calcularPesoAfinidade(Jogador jogador1, Jogador jogador2) {
         if (jogador1.getClube().equals(jogador2.getClube()) && jogador1.getNacionalidade().equals(jogador2.getNacionalidade())) {
             return 3;
@@ -36,9 +43,71 @@ public class Grafo {
         }
     }
 
+    //Imprime as compatibilidades
     public void mostrarCompatibilidades() {
         for (Aresta aresta : compatibilidades) {
             System.out.println("Compatibilidade entre " + aresta.getInicio().getNome() + " e " + aresta.getFim().getNome() + ": " + aresta.getPeso());
         }
+    }
+
+    //Remove um jogador de acordo com seu nome.
+    public void removerJogador(String nomeJogador) {
+        Iterator<Jogador> iterator = jogadores.iterator();
+        while (iterator.hasNext()) {
+            Jogador jogador = iterator.next();
+            if (jogador.getNome().equals(nomeJogador)) {
+                // Remove o jogador
+                iterator.remove();
+                // Remove as arestas associadas ao jogador
+                Iterator<Aresta> arestaIterator = compatibilidades.iterator();
+                while (arestaIterator.hasNext()) {
+                    Aresta aresta = arestaIterator.next();
+                    if (aresta.getInicio().equals(jogador) || aresta.getFim().equals(jogador)) {
+                        arestaIterator.remove();
+                    }
+                }
+                System.out.println("Jogador " + nomeJogador + " removido com sucesso.");
+                return;
+            }
+        }
+        System.out.println("Jogador " + nomeJogador + " não encontrado.");
+    }
+
+    //Lista todos os jogadores adicionados.
+    public void listarJogadores() {
+        System.out.println("Lista de Jogadores:");
+        for (Jogador jogador : jogadores) {
+            System.out.println("Nome: " + jogador.getNome() +
+                    ", Nacionalidade: " + jogador.getNacionalidade() +
+                    ", Clube: " + jogador.getClube());
+        }
+    }
+
+    //Lista os jogadores filtrando por clube.
+    public List<Jogador> encontrarPorClube(String clube) {
+        List<Jogador> jogadoresNoClube = new ArrayList<>();
+        for (Jogador jogador : jogadores) {
+            if (jogador.getClube().equalsIgnoreCase(clube)) {
+                jogadoresNoClube.add(jogador);
+            }
+        }
+        return jogadoresNoClube;
+    }
+
+    //Calcula a afinidade total do time, somando todas as afinidades e dividindo pela qtd das mesmas.
+    public double calcularAfinidadeTotal() {
+        double somaAfinidades = 0;
+        int numeroCombinacoes = (jogadores.size() * (jogadores.size() - 1)) / 2;
+
+        for (Aresta aresta : compatibilidades) {
+            somaAfinidades += aresta.getPeso();
+        }
+
+        return somaAfinidades / numeroCombinacoes;
+    }
+
+    //Limpa as compatibilidades para nao imprimir repetido.
+    public void limparCompatibilidades() {
+        compatibilidades.clear();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,78 @@
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    //Criacao de um menu que chama os metodos criados
     public static void main(String[] args) {
         Grafo grafo = new Grafo();
+        Scanner scanner = new Scanner(System.in);
 
-        grafo.adicionarJogador("Neymar", "Brasileiro", "PSG");
-        grafo.adicionarJogador("Messi", "Argentino", "PSG");
-        grafo.adicionarJogador("Mbappé", "Francês", "PSG");
-        grafo.adicionarJogador("Cristiano Ronaldo", "Português", "Manchester United");
-        grafo.adicionarJogador("Marquinhos","Brasileiro","PSG");
+        int opcao;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1 - Adicionar jogador");
+            System.out.println("2 - Remover jogador");
+            System.out.println("3 - Listar os jogadores adicionados");
+            System.out.println("4 - Filtrar jogadores por clube");
+            System.out.println("5 - Mostrar a compatibilidade entre todos os jogadores");
+            System.out.println("6 - Mostrar a compatibilidade do time no geral");
+            System.out.println("7 - Sair");
+            System.out.print("Escolha uma opção: ");
 
-        grafo.calcularAfinidade();
-        grafo.mostrarCompatibilidades();
+            opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer do scanner
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Nome do jogador: ");
+                    String nome = scanner.nextLine();
+                    System.out.print("Nacionalidade do jogador: ");
+                    String nacionalidade = scanner.nextLine();
+                    System.out.print("Clube do jogador: ");
+                    String clube = scanner.nextLine();
+                    grafo.adicionarJogador(nome, nacionalidade, clube);
+                    break;
+                case 2:
+                    System.out.print("Nome do jogador a ser removido: ");
+                    String nomeRemover = scanner.nextLine();
+                    grafo.removerJogador(nomeRemover);
+                    break;
+                case 3:
+                    grafo.listarJogadores();
+                    break;
+                case 4:
+                    System.out.print("Nome do clube para filtrar jogadores: ");
+                    String clubeFiltro = scanner.nextLine();
+                    List<Jogador> jogadoresFiltrados = grafo.encontrarPorClube(clubeFiltro);
+                    System.out.println("Jogadores do clube " + clubeFiltro + ":");
+                    for (Jogador jogador : jogadoresFiltrados) {
+                        System.out.println("Nome: " + jogador.getNome() +
+                                ", Nacionalidade: " + jogador.getNacionalidade() +
+                                ", Clube: " + jogador.getClube());
+                    }
+                    break;
+                case 5:
+                    grafo.limparCompatibilidades();
+                    grafo.calcularAfinidade();
+                    grafo.mostrarCompatibilidades();
+                    break;
+                case 6:
+                    double afinidadeTotal = grafo.calcularAfinidadeTotal();
+                    DecimalFormat df = new DecimalFormat("#.##");
+                    System.out.println("Afinidade total do time: " + df.format(afinidadeTotal));
+                    break;
+                case 7:
+                    System.out.println("Saindo do programa. Até mais!");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha novamente.");
+                    break;
+            }
+
+        } while (opcao != 7);
+
+        scanner.close();
     }
 }
